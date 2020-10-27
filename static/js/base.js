f3bb8af3a4519ecac18259d3fc87fa5741be3389
JihanePett <jihane.petticrew@gmail.com>
2020-10-27 13:40:48 +0000

    fixed timeline bug

--- static/js/base.js
+++ static/js/base.js
@@ -3,9 +3,9 @@ tl.staggerFrom(".topics", 3, {
   css:{transform:"scale(0)",top:"60%",left:"50%"},ease:Elastic.easeOut
 }, 0.3);
 
-var js = document.getElementById("js");
-var sass = document.getElementById("sass");
-var php = document.getElementById("php");
-var css3 = document.getElementById("css3");
-var hmtl5 = document.getElementById("html5");
+var about = document.getElementById("about");
+var portfolio = document.getElementById("portfolio");
+var contact = document.getElementById("contact");
+var dl = document.getElementById("dl");
+var exposure = document.getElementById("exposure");
 var topics = document.getElementsByClassName("topics");
