a73eedba39b95866ecae9dd1faaac4524f7ee486
JihanePett <jihane.petticrew@gmail.com>
2020-11-11 19:13:26 +0000

    added about and timeline view url and templates

--- home/views.py
+++ home/views.py
@@ -6,3 +6,13 @@ from django.shortcuts import render
 def index(request):
     # A view to return the index page
     return render(request, 'home/index.html')
+
+
+def about(request):
+    # A view to return the about page
+    return render(request, 'home/about.html')
+
+
+def timeline(request):
+    # A view to return the about page
+    return render(request, 'home/timeline.html')
